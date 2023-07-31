802e6d05facba95268f0636133b0cc5bb1386960
Mathieu Broeckhoven <mathieu_broe@yahoo.com>
2023-07-31 21:21:16 +0200

    Added modstream

---
+++ ModManager/Streaming/ModStream.cs
@@ -0,0 +1,28 @@
+﻿using ModManager.Extensions;
+using ModManager.Models;
+
+namespace ModManager.Streaming;
+
+public class ModStream
+{
+    private List<Mod> _mods = null;
+    private const string ModsFolder = $"C:\\Users\\mathi\\Documents\\My Games\\FarmingSimulator2022\\mods\\modManagerTestModsFolder";
+    private const string ModNamePrefix = "FS22_";
+    
+    
+    //method returns all zip-file names
+    public List<Mod> GetMods()
+    {
+        if (_mods is null || _mods.Count == 0)
+        {
+            var fileNames = Directory.GetFiles(ModsFolder);
+            _mods = fileNames.Select(name => new Mod(name.TrimModName(ModNamePrefix))).ToList();
+        }
+        return _mods;
+    }
+    
+    public Mod GetMod(string modName)
+    {
+        return GetMods().FirstOrDefault(mod => mod.Name == modName)!;
+    }
+}
